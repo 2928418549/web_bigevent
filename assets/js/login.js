a6a3927cb7643f8ea136ee268bff71e90bf11f86
tao <2928418549@qq.com>
2020-11-23 19:51:02 +0800

    完成了登录与注册的页面

---
+++ assets/js/login.js
@@ -0,0 +1,58 @@
+$(function () {
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+    $('#link_login').on('click', function () {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+    //form.verify
+    var form = layui.form
+    var layer = layui.layer
+    form.verify({
+        pass: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ],
+        repwd: function (value) {
+            var pwd = $(".reg-box [name=password]").val()
+            if (pwd !== value) {
+                return '两次输入不一致'
+            }
+        }
+    })
+    //给注册监听事件
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault()
+        var data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        $.post('/api/reguser', data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功，请登录！')
+            $('#link_login').click()
+        })
+    })
+    //给登录监听事件
+    $('#form_login').on('submit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/api/login',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg('登录成功')
+                console.log(res);
+                localStorage.setItem('token', res.token)
+                location.href = '/index.html'
+            }
+        })
+    })
+})
